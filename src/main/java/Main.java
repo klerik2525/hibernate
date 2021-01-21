3df227e23868e46655b4cb04a29d4c690e578040
klerik2525 <64359472+klerik2525@users.noreply.github.com>
2021-01-21 16:35:28 +0300

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,27 @@
+import services.UserService;
+
+import java.sql.SQLException;
+
+public class Main {
+    public static void main(String[] args) throws SQLException {
+
+        UserService userService = new UserService();
+       /*User user = new User("Masha", 26);
+        userService.saveUser(user);
+        Auto ferrari = new Auto("Ferrari", "red");
+        Auto ford = new Auto("Ford", "black");
+        user.addAuto(ferrari);
+        Auto opel = new Auto("Opel", "red");
+        Auto bmw = new Auto("BMW", "blue");
+        user.addAuto(opel);
+        user.addAuto(bmw);
+        ford.setUser(user);
+        user.addAuto(ford);
+        userService.updateUser(user);*/
+
+
+        //System.out.print(userService.findUser(81).getAutos() + user.getName());
+
+       userService.deleteOfId(87);
+    }
+}
