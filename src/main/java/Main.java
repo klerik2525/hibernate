59b97ba52f10dd0da41fa2bcdcc19e22dd26188b
klerik2525 <64359472+klerik2525@users.noreply.github.com>
2021-02-02 12:44:28 +0300

    refactor

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,15 +1,10 @@
-import dao.UserDao;
 import models.Auto;
 import models.User;
-import org.hibernate.Session;
-import org.hibernate.query.Query;
 import services.UserService;
-import utils.HibernateSessionFactoryUtil;
+
 
 import java.sql.SQLException;
-import java.util.Arrays;
-import java.util.Iterator;
-import java.util.List;
+
 
 public class Main {
     public static void main(String[] args) throws SQLException {
@@ -37,17 +32,6 @@ public class Main {
         //System.out.print(userService.findAllAuto());
         userService.findUsersCars();
 
-      /*  Session session = HibernateSessionFactoryUtil.getSessionFactory().openSession();
-        String sql = "SELECT p.id, p.name , ps.user_id, ps.model "  +  "FROM users p "  +
-                "INNER JOIN autos ps ON ps.User_id = p.id";
-        Query query = (Query) session.createSQLQuery(sql);
-        List l = query.list();
-        Iterator it = l.iterator();
-        while (it.hasNext()) {
-            Object rows[] = (Object[]) it.next();
-            System.out.println(rows[0] + "--" + rows[1] + "--" + rows[3] + "--");
 
-
-        }*/
     }
 }
